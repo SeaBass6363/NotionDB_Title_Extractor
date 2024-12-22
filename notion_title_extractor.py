c86b30839b360de6b648b53e9fd27821dc124382
Hardwood Vazquez <s.vazquez6363@gmail.com>
2024-12-21 23:43:53 -0500

    Update regex in script
    Took out individual regex to remove certain parts of the string to one compiled regex that searches for the needed expression and one that removes the asterisks

--- notion_title_extractor.py
+++ notion_title_extractor.py
@@ -16,16 +16,28 @@ input_file.close()
 # Opens output file for editing
 output_file = open(output_file_path, "w")
 
+# Compiled regex expression to extract the string in between brackets 
+# or followed by a hashtag and space
+txtRegex = re.compile(r'((?<=(^#\s)).*)|((?<=(\[)).*(?=\]))')
+#counter to number each line
+counter = 1
+
 for line in content:
     # Initiates cleaned_line without any leading spaces
     cleaned_line = line.lstrip()
-    # Removes asterisks (*), brackets ([]), and parentheses ()
-    cleaned_line = re.sub(r'[\*\[\]\(\)]', '', cleaned_line)
-    # Removes hashtags (#) and their immediately followed spaces
-    cleaned_line = re.sub(r'#\s', '', cleaned_line)
-    # Removes URLs (matching typical HTTP/HTTPS links)
-    cleaned_line = re.sub(r'http[s]?://\S+', '', cleaned_line)
-        
+    # Removes asterisks (*)
+    cleaned_line = re.sub(r'[\*]', '', cleaned_line)
+    # Matches pattern from cleaned_line into str match
+    match = re.search(txtRegex, cleaned_line)
+    # If match is successful cleaned_line is numbered, updated with matched string, 
+    # and a newline is inputted at the end
+    if match:
+        cleaned_line = str(counter)+ ". " + match.group() + "\n"
+        #counter increases by one, every successful match
+        counter += 1
+        #prints the new cleaned line before written on the output file
+        print(cleaned_line) 
+
     # Write the cleaned line to the output file
     output_file.write(cleaned_line)
     
